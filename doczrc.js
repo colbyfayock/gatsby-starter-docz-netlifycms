cb9057fa5646e93f5bd6b2a012fc1a1e60758513
Colby Fayock <fayock@gmail.com>
2020-02-11 20:04:13 -0500

    initial commit

---
+++ doczrc.js
@@ -0,0 +1,13 @@
+export default {
+  files: 'src/**/*.{md,markdown,mdx}',
+  title: 'My Awesome Documentation',
+  menu: [
+    'Welcome',
+    {
+      name: 'Getting Started',
+      menu: [
+        'Installation'
+      ]
+    }
+  ]
+}
