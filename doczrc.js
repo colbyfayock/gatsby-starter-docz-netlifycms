28c7c6d815d959fc62bc0d2fce0712846be2a8c1
colbyfayock <fayock@gmail.com>
2020-08-22 09:32:44 -0400

    package upgrades, shortening title

--- doczrc.js
+++ doczrc.js
@@ -4,6 +4,6 @@ const { menuItems } = require(`${projectRoot}/config/menu.json`);
 
 export default {
   files: 'docs/**/*.{md,markdown,mdx}',
-  title: 'Gatsby Starter with Docz & Netlify CMS',
+  title: 'Docz & Netlify CMS',
   menu: menuItems
 }
